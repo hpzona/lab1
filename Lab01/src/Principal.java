cf88006028d6ef23cd91eed1b7b80ac40c65bd37
Administrador <Administrador@s06m07.liict.ufsc.br>
2013-08-21 21:15:46 -0300

    asdas

--- Lab01/src/Principal.java
+++ Lab01/src/Principal.java
@@ -1,8 +1,22 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 
 public class Principal {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int  poolLeitores = 4;
+		int  poolEscritores = 1;
+		
+		
+		//Leitor
+		ExecutorService te  =  Executors.newFixedThreadPool(poolLeitores);
+		ScheduledExecutorService  ste = Executors.newScheduledThreadPool(poolEscritores);
+		
+		ste.scheduleAtFixedRate(new Escritor("Escritor-scheduled-At-Fixed-Rate", 10, 10000, TimeUnit.NANOSECONDS);
+		
 
 	}
 
